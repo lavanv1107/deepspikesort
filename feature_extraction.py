1e418c7a3ffd7613f3d4ff009eb6a49b85b7e88e
Vatanak Lavan <vatanaklavan@proton.me>
2023-11-23 13:12:07 -0500

    Renamed script file
    Renamed feature extraction script to "feature_extraction"

--- feature_extraction.py
+++ feature_extraction.py
@@ -3,7 +3,7 @@ import os
 import torch
 import torch.nn as nn
 
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
@@ -20,11 +20,10 @@ def extract_features(dataloader, feature_dim, model, device, device_ids=None):
     num_samples = len(dataloader.dataset)
     
     features = torch.zeros(num_samples, feature_dim).to(device)
-    folders = []
     
     start_idx = 0
     with torch.no_grad():
-        for batch_idx, (inputs, _) in enumerate(tqdm(dataloader, desc="Extracting Features", dynamic_ncols=True)):
+        for batch_idx, inputs in enumerate(tqdm(dataloader, desc="Extracting Features", dynamic_ncols=True)):
             inputs_dict = {'x': inputs, 'feature_extraction': True}
             outputs = model(inputs_dict)
             
@@ -32,24 +31,13 @@ def extract_features(dataloader, feature_dim, model, device, device_ids=None):
             features[start_idx:end_idx] = outputs
             start_idx = end_idx
             
-            dataset = dataloader.dataset.dataset
-            
-            # Get folder names using the get_image_folder method
-            batch_folders = [
-                dataset.get_label(idx)
-                for idx in range(batch_idx * dataloader.batch_size, (batch_idx + 1) * dataloader.batch_size)
-                if idx < len(dataloader.dataset)
-            ]
-            
-            folders.extend(batch_folders)
-            
     # Move final features tensor to CPU and convert to numpy
     features = features.cpu().numpy()
     
-    return features, folders
+    return features
 
 
-def preprocess_features(features, n_components=100):
+def preprocess_features(features, n_components):
     # Perform PCA dimensionality reduction
     pca = PCA(n_components=n_components)
     embeddings_pca = pca.fit_transform(features)
