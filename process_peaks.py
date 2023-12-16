a34a6ae9ea7a5e04e6cd4fbf68aa59c2bed15bdd
Vatanak Lavan <vatanaklavan@proton.me>
2023-12-16 00:23:16 -0500

    Updated script for processing peaks
    - Removed saving and loading functions
    - Modifed to use numpy arrays instead of pandas dataframes

--- process_peaks.py
+++ process_peaks.py
@@ -7,35 +7,9 @@ from numba import jit
 from tqdm import tqdm
 
 
-def save_peaks(peaks, folder):
+def filter_peaks(recording, peaks):
     """
-    Saves peaks object to specified folder path in disk.
- 
-    Args:
-        peaks (obj): A peaks object returned by SpikeInterface's detect_peaks method.
-        folder (str): A folder path name.
-    """
-    os.mkdir(folder)
-    np.save(os.path.join(folder, 'peaks.npy'), peaks)
-
-    
-def load_peaks(folder):
-    """
-    Loads peaks object from specified folder path in disk.
- 
-    Args:
-        folder (str): A folder path name.
- 
-    Returns:
-        obj: A peaks object loaded from disk. 
-    """
-    return np.load(os.path.join(folder, 'peaks.npy'))
-
-
-def extract_peaks(recording, peaks):
-    """
-    Creates a table using information from a peaks object.
-    Peaks which do not fit in with the time frame format (centered between 31 and 33 frames) will be dropped.
+    Filters peaks which do not fit in with the time frame format (centered between 31 and 33 frames) will be dropped.
  
     Args:
         recording (obj): A RecordingExtractor object created from an NWB file using SpikeInterface.
@@ -44,22 +18,29 @@ def extract_peaks(recording, peaks):
     Returns:
         obj: A table containing peaks information.
     """
-    peaks_table = pd.DataFrame(peaks)
-    
-    peaks_table.rename(columns={'sample_index': 'peak_frame', 'channel_index':'peak_channel'}, inplace=True)
-    peaks_table.drop(columns=['amplitude','segment_index'], inplace=True)
+    # Columns to keep
+    columns_filtered = ['sample_index', 'channel_index']
 
+    # Define a new dtype with only the desired columns
+    dtype_filtered = np.dtype([(name, peaks.dtype[name]) for name in columns_filtered])
+
+    # Create a new array with the new dtype
+    peaks_filtered = np.zeros(peaks.shape, dtype=dtype_filtered)
+
+    # Copy data from the old array to the new array
+    for name in dtype_filtered.names:
+        peaks_filtered[name] = peaks[name]
+    
     # Create a boolean mask to identify rows within the specified range
-    mask = (peaks_table['peak_frame'] >= 31) & (peaks_table['peak_frame'] <= recording.get_num_frames() - 33)
+    mask_filtered = (peaks['sample_index'] >= 31) & (peaks['sample_index'] <= recording.get_num_frames() - 33)
 
-    peaks_table = peaks_table[mask]
-    peaks_table = peaks_table.reset_index(drop=True)
+    peaks_filtered = peaks_filtered[mask_filtered]
     
-    return peaks_table
+    return peaks_filtered
     
     
 @jit
-def match_peaks(peaks_table, spikes_table, channels_table):
+def match_peaks(peaks, spikes, channels):
     """
     There are peaks that were detected which are spikes. We can verify this using the spikes information within the NWB file.
     However, there are peaks which are not exact spike matches but they are very close and can be considered as such.
@@ -78,37 +59,33 @@ def match_peaks(peaks_table, spikes_table, channels_table):
         channels_table (obj): A table of channel locations.
  
     Returns:
-        obj: A table containing peaks information with unit ids.
+        obj: An array containing peaks information with unit ids.
     """
-    # Convert peaks and spikes data to numpy arrays
-    peaks_array = peaks_table[['peak_frame', 'peak_channel']].values
-    spikes_array = spikes_table[['unit_id', 'peak_frame', 'peak_channel']].values
-    channels_array = channels_table[['channel_loc_x', 'channel_loc_y']].values
-
     # Create an array to store matches
-    matches = -1 * np.ones(peaks_array.shape[0], dtype=int)
+    matches = -1 * np.ones(peaks.shape[0], dtype=int)
 
     # Iterate through peaks_array using indices 
-    for i, peak in tqdm(enumerate(peaks_array), total=peaks_array.shape[0], desc="matching peaks", dynamic_ncols=True):
+    for i, peak in tqdm(enumerate(peaks), total=peaks.shape[0], desc="matching peaks to spikes", dynamic_ncols=True):
         # Find the starting index for possible matches using binary search
-        start_index = np.searchsorted(spikes_array[:, 1], peak[0] - 4)
+        start_index = np.searchsorted(spikes['sample_index'], peak[0] - 4)
 
         # Get the location of the current peak's channel
-        peak_loc = channels_array[peak[1]]
+        peak_loc = channels[peak[1]]
 
         # Set the least amount of distance between the current peak and spike channels
         least_distance = 100 # in um
 
         # Iterate through potential matching spikes
-        for x in range(start_index, spikes_array.shape[0]):
-            spike_time = spikes_array[x, 1]
+        for x in range(start_index, spikes.shape[0]):
+            spike_time = spikes['sample_index'][x]
 
             # Exit the loop if spike_time exceeds the upper bound
             if spike_time > peak[0] + 4:
                 break
 
             # Get the location of the current spike's channel
-            spike_loc = channels_array[spikes_array[x, 2]]
+            spike_channel = spikes['channel_index'][x]
+            spike_loc = channels[spike_channel]
 
             # Calculating the distance in space and time
             # This gives equal weight between 1 um and 1 time sample
@@ -118,15 +95,29 @@ def match_peaks(peaks_table, spikes_table, channels_table):
 
             # Set a unit_id for the peak if a spike is closest in distance to the current peak
             if distance < least_distance:
-                matches[i]= spikes_array[x, 0]
+                matches[i]= spikes['unit_index'][x]
                 least_distance = distance
 
-    # Create the matched table
-    peaks_matched_table = pd.DataFrame(peaks_array, columns=['peak_frame', 'peak_channel'])
-    peaks_matched_table['unit_id'] = matches  # Add 'unit_id' column
-    peaks_matched_table.insert(0, 'unit_id', peaks_matched_table.pop('unit_id'))
+    # Existing column names
+    columns_peaks = list(peaks.dtype.names)
 
-    return peaks_matched_table
+    # New order of columns including the new column
+    order_matched = columns_peaks[:0] + ['unit_index'] + columns_peaks[0:]
+
+    # Define a new dtype with columns in the new order
+    dtype_matched = [(name, peaks.dtype[name]) if name in columns_peaks else (name, '<i8') for name in order_matched]
+
+    # Create a new array with the new dtype
+    peaks_matched = np.zeros(peaks.shape, dtype=dtype_matched)
+
+    # Copy data from the old array to the new array
+    for name in columns_peaks:
+        peaks_matched[name] = peaks[name]
+        
+    # Fill the new column with matches
+    peaks_matched['unit_index'] = matches
+
+    return peaks_matched
     
     
 def get_peaks_spikes(peaks_matched_table):
