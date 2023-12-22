daa6224ee4505719fcbaf48925b10b97030a2c45
Vatanak Lavan <vatanaklavan@proton.me>
2023-12-22 04:53:33 -0500

    Updated training and validation
    - Added better logging
    - Added early stopping function
    - Added new functions for plotting validation metrics and confusion matrix

--- training.py
+++ training.py
@@ -1,18 +1,23 @@
 import os
 
+import time
+
+import numpy as np
+
 import torch
 import torch.nn as nn
 
-import time
-from tqdm import tqdm
-
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+import logging
 
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import seaborn as sns
 
-from util import AverageMeter 
+import util 
 
 
 class TrainModel():
@@ -21,9 +26,9 @@ class TrainModel():
     It utilizes DataParallel for training on multiple GPUs.
     """
 
-    def __init__(self, device, device_ids, loss_fn, optimizer, train_dataloader=None, test_dataloader=None):
+    def __init__(self, device, device_ids, loss_fn, optimizer, train_dataloader=None, val_dataloader=None, verbose_count=25):
         """
-        Initializes the training and testing setup.
+        Initializes the training and validation setup.
 
         Args:
             device (str): The device on which to run the model (e.g., 'cuda:0').
@@ -31,29 +36,29 @@ class TrainModel():
             loss_fn (torch.nn.modules.loss): The loss function to use for training.
             optimizer (torch.optim.Optimizer): The optimizer to use for training.
             train_dataloader (torch.utils.data.DataLoader, optional): DataLoader for the training data.
-            test_dataloader (torch.utils.data.DataLoader, optional): DataLoader for the testing data.
+            val_dataloader (torch.utils.data.DataLoader, optional): DataLoader for the validation data.
         """
         self.train_dataloader = train_dataloader
-        self.test_dataloader = test_dataloader
+        self.val_dataloader = val_dataloader
         self.device = device
         self.device_ids = device_ids
         self.loss_fn = loss_fn
         self.optimizer = optimizer
+        self.verbose_count = verbose_count
 
-    def train(self, model, epoch, verbose_interval=10):
+    def train(self, model, epoch):
         """
         Args:
-            model (torch.nn.Module): The PyTorch model to train.
-            epoch (int): The current epoch number.
-            verbose_interval (int, optional): Interval (in number of batches) at which to print detailed training progress. 
-                                               If set to 0 or None, no detailed progress will be printed. Defaults to 5.
+            model (torch.nn.Module): PyTorch model to train.
+            epoch (int): Current epoch number.
+            verbose_count (int, optional): Number of times to print detailed progress.
 
         Returns:
             float: The average loss for an epoch.
         """    
-        loss_meter = AverageMeter()
-        batch_time_meter = AverageMeter()
-        data_time_meter = AverageMeter()
+        loss_meter = util.AverageMeter()
+        batch_time_meter = util.AverageMeter()
+        data_time_meter = util.AverageMeter()
         
         # Start training the model
         model.to(self.device)
@@ -61,7 +66,10 @@ class TrainModel():
         model.train()
 
         # Iterate through each batch in the training dataloader
-        end = time.time()
+        total_batches = len(self.train_dataloader)
+        verbose_interval = max(1, total_batches // self.verbose_count)
+        
+        end = time.time()        
         for batch, (X, Y) in enumerate(self.train_dataloader):
             data_time_meter.update(time.time() - end)
 
@@ -88,166 +96,239 @@ class TrainModel():
             batch_time_meter.update(time.time() - end)
             end = time.time()
             
-            if verbose_interval and (batch % verbose_interval) == 0:
-                print('- Epoch: [{0}][{1}/{2}]\t'
+            if batch % verbose_interval == 0:
+                print('Epoch: [{0}][{1}/{2}]\t'
                       'Time: {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                       'Data: {data_time.val:.3f} ({data_time.avg:.3f})\t'
                       'Loss: {loss.val:.4f} ({loss.avg:.4f})'
-                      .format(epoch+1, batch, len(self.train_dataloader), batch_time=batch_time_meter,
+                      .format(epoch, batch, total_batches, batch_time=batch_time_meter,
                               data_time=data_time_meter, loss=loss_meter))
             
         return loss_meter.avg
         
-    def test(self, model):
+    def validate(self, model, epoch):
         """
-        This tests/validates the model and prints the model's accuracy and average loss for an epoch.
+        This validates the model and prints the model's accuracy and average loss for an epoch.
         It also returns the true labels and predicted labels for an epoch as well.
 
         Args:
-            model (obj): A PyTorch model.
+            model (torch.nn.Module): A PyTorch model.
             
         Returns:
-            test_loss (int): The average test loss for an epoch.
-            true_labels (obj): A list of the actual true labels being used for training.
-            predicted_labels (obj): A list of the model's predicted labels when validating.
-        """
-        # Get the total number of samples in the test dataset
-        size = len(self.test_dataloader.dataset)
+            val_loss (float): The average validation loss for an epoch.
+            true_labels (list): The actual true labels.
+            predicted_labels (list): The model's predicted labels.
+        """  
+        loss_meter = util.AverageMeter()
+        batch_time_meter = util.AverageMeter()
+        data_time_meter = util.AverageMeter()
         
-        # Set the model to evaluation mode (no gradient updates during evaluation)        
+        # Set the model to evaluation mode
         model.to(self.device)
         model = nn.DataParallel(model, device_ids=self.device_ids)
         model.eval()
 
-        # Initialize variables to keep track of test loss and correct predictions
-        test_loss, correct = 0, 0
-
-        # Lists to store true labels and predicted labels for later analysis
+        # Lists to store true and predicted labels
         true_labels = []
         predicted_labels = []
+        
+        # Total number of samples and batches for verbosity control
+        total_batches = len(self.val_dataloader)
+        verbose_interval = max(1, total_batches // self.verbose_count)  # Control print frequency
 
-        # Disable gradient computation for efficiency during evaluation
-        with torch.no_grad(), tqdm(total=len(self.test_dataloader), desc='Testing', dynamic_ncols=True) as progress_bar:
-            # Iterate through each batch in the test dataloader
-            for batch, (X, Y) in enumerate(self.test_dataloader):
+        # Start timing
+        end = time.time()
+        for batch, (X, Y) in enumerate(self.val_dataloader):
+            # Update data loading time
+            data_time_meter.update(time.time() - end)
 
-                # Move input data and labels to the specified device (e.g., GPU)
-                X, Y = X.to(self.device), Y.to(self.device)
+            # Move data to the specified device
+            X, Y = X.to(self.device), Y.to(self.device)
 
-                # Forward pass: Compute predictions using the model
-                inputs_dict = {'x': X}
-                pred = model(inputs_dict)
+            # Forward pass: compute predictions
+            inputs_dict = {'x': X}
+            pred = model(inputs_dict)
 
-                # Compute the test loss and add it to the running total
-                test_loss += self.loss_fn(pred, Y).item()
+            # Compute loss
+            loss = self.loss_fn(pred, Y)
+            loss_meter.update(loss.item(), X.size(0))
 
-                # Count correct predictions by comparing predicted labels to true labels
-                correct += (pred.argmax(1) == Y).type(torch.float).sum().item()
+            # Update batch processing time
+            batch_time_meter.update(time.time() - end)
+            end = time.time()
 
-                # Store the true labels and predicted labels for further analysis
-                true_labels.extend(Y.tolist())
-                predicted_labels.extend(pred.argmax(1).tolist())
+            # Store labels for analysis
+            true_labels.extend(Y.tolist())
+            predicted_labels.extend(pred.argmax(1).tolist())
 
-                # Update the progress bar
-                progress_bar.update()
+            # Print progress
+            if batch % verbose_interval == 0:
+                print('Epoch: [{0}][{1}/{2}]\t'
+                      'Time: {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Data: {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                      'Loss: {loss.val:.4f} ({loss.avg:.4f})'
+                      .format(epoch, batch, total_batches, batch_time=batch_time_meter,
+                              data_time=data_time_meter, loss=loss_meter))
+                
+        # Calculate accuracy
+        correct_predictions = sum([1 for true, pred in zip(true_labels, predicted_labels) if true == pred])
+        accuracy = correct_predictions / len(true_labels) if true_labels else 0
 
-        # Average test loss over the number of batches
-        test_loss /= size
-        correct /= size
-
-        # Print the test results (accuracy and average loss)
-        print(f'\nTest Error:\n- Accuracy: {(100 * correct):>0.1f}%\n- Average Loss: {test_loss:>8f}\n')
-
-        # Return the test loss, true labels, and predicted labels for external analysis
-        return test_loss, true_labels, predicted_labels
+        return loss_meter.avg, accuracy, true_labels, predicted_labels
 
 
-    def train_test_model(self, model, model_name, models_folder, epochs, classes):
+    def train_validate(self, model, num_epochs, labels_map, session_name, results_folder):
         """
-        This trains and tests a model.
+        This trains and validates a model.
         It plots and saves loss and accuracy over time as well as a confusion matrix for the model's predictions.
         There is also a checkpoint saving functionality which allows for pausing the training which saves the model's 
         current state, current epoch and accumulated losses and accuriacies.
 
         Args:
             model (obj): A PyTorch model
-            model_name (str): A given name for the model.
-            models_folder (str): The folder path name containing the model.
+            session_name (str): A given name for the model.
+            results_folder (str): The folder path name containing the model.
             epochs (int): The number of epochs to train the model.
             classes (int): The classes in the dataset.
         """
-        model, start_epoch, losses, accuracies = load_checkpoint(model, model_name, models_folder)
+        metrics_progress_file = os.path.join(results_folder, f'{session_name}_metrics_progress.log')        
+        logger_class = logging.getLogger('logger_class')
+        logger_class.setLevel(logging.INFO)
+        file_handler_class = logging.FileHandler(metrics_progress_file, mode='w')
+        formatter_class = logging.Formatter('%(asctime)s - %(message)s')
+        file_handler_class.setFormatter(formatter_class)
+        logger_class.addHandler(file_handler_class)
         
-        for epoch in range(start_epoch, epochs):
-            # print(f'Epoch {epoch+1}\n-------------------------------')
-
-            # Train the model and get the losses for this epoch
-            self.train(model)
-
-            # Test the model and get the loss and accuracy for this epoch
-            test_loss, true_labels, predicted_labels = self.test(model)
-            
-            cm = confusion_matrix(true_labels, predicted_labels, normalize='all')
-            plt.figure(figsize=(30, 30))
-            sns.heatmap(cm, annot=True, fmt=".2f", cmap='Blues', xticklabels=classes, yticklabels=classes)
-            plt.title(f'Confusion Matrix ({epoch+1} Epochs)')
-            plt.xlabel('Predicted')
-            plt.ylabel('True')
-            plt.savefig(os.path.join(models_folder, f'{model_name}_confusion_matrix.png'))
+        losses = []
+        accuracies = []
         
-        print('Training completed.\n')
+        best_loss = float('inf')
+        best_epoch = 0
+        patience_counter = 0
+        patience_limit = 10
+        improvement_threshold = 0.01
+    
+        total_end = time.time()  
+        for epoch in range(1, num_epochs+1):           
+                print(util.write_separator())
+                print(f"Epoch {epoch}")  
+                print(util.write_separator())
+
+                # Train the model and get the losses for this epoch
+                print("- Training")
+                print('Training model...')
+                
+                train_end = time.time()
+                
+                self.train(model, epoch)
+                
+                print(f'\nTraining time: {time.time() - train_end:.3f}\n')
+
+                # Validate the model and get the loss and accuracy for this epoch
+                print("- Validation")
+                print('Validating model...')
+                
+                val_end = time.time()
+                
+                val_loss, val_acc, true_labels, predicted_labels = self.validate(model, epoch)
+                losses.append(val_loss)
+                accuracies.append(val_acc)
+                
+                log_metric_progress(logger_class, val_loss, val_acc, epoch, results_folder)  
+                
+                print(f'\nValidation time: {time.time() - val_end:.3f}')
+                
+                # Check for improvement
+                if val_loss < best_loss * (1 - improvement_threshold):
+                    best_loss = val_loss
+                    best_epoch = epoch
+                    patience_counter = 0
+                else:
+                    patience_counter += 1
+
+                # Early stopping condition
+                if patience_counter >= patience_limit:
+                    break
+                
+        print(f"\nTotal time: {time.time() - total_end:.3f}")
+        
+        # Visualize the changes in cluster assignments   
+        metrics = {
+            'Accuracy': accuracies,           
+        }
+        plot_metric_progress(metrics, epoch, session_name, results_folder)
+        plot_confusion_matrix(true_labels, predicted_labels, labels_map, session_name, results_folder)
 
 
-def save_checkpoint(model, model_name, models_folder, end_epoch, losses, accuracies):
+def log_metric_progress(logger_dss, loss, accuracy, epoch, results_folder):       
+    logger_dss.info('Epoch: [{0}]\t'
+                    'Loss: {1:.4f}\t'
+                    'Accuracy: {2:.4f}'
+                    .format(epoch, loss, accuracy))
+    
+
+def plot_metric_progress(metrics, num_epochs, session_name, results_folder):
+    # Calculate the width of the figure based on the number of epochs
+    base_width = 10
+    dynamic_width = (num_epochs / 100) * base_width
+
+    # Ensure a minimum width is maintained
+    dynamic_width = max(base_width, dynamic_width)
+
+    # Number of subplots
+    num_metrics = len(metrics)
+    fig, axs = plt.subplots(num_metrics, 1, figsize=(dynamic_width, 5 * num_metrics))
+
+    if num_metrics == 1:
+        axs = [axs]
+
+    for ax, (metric_type, metric_info) in zip(axs, metrics.items()):
+        # Plot each metric
+        ax.plot(range(1, num_epochs + 1), metric_info, marker='o', color='r')
+        ax.set_title(f'{metric_type}')
+        ax.set_xlabel('Epoch')
+        ax.set_ylabel(metric_type)
+        ax.grid(True)
+
+        ax.set_xlim(1, num_epochs)
+        ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+
+    plt.tight_layout()
+    plt.savefig(os.path.join(results_folder, f"{session_name}_metrics_progress.png"))
+    plt.close(fig)
+    
+        
+def plot_confusion_matrix(true_labels, predicted_labels, labels_map, session_name, results_folder):
     """
-    Creates a dictionary of checkpoint variables for a model.
-    This inlcudes the model's state dictionary, the epoch in which it finished/stopped training, and the accumulated losses and accuracies from training.
-    The checkpoint will be saved to disk as a PyTorch pt file.
+    Plots a confusion matrix based on true and predicted labels.
 
     Args:
-        model (obj): A PyTorch model.
-        model_name (str): A given name for the model.
-        models_folder (str): The folder path name containing the model.
-        end_epoch (int): The epoch when training was finished/stopped.
-        losses (obj): A list of losses per epoch from training.
-        accuracies (obj): A list of accuracies per epoch from training.
+        true_labels (list): List of true numerical labels.
+        predicted_labels (list): List of predicted numerical labels.
+        labels_map (dict): Dictionary mapping numerical labels to class names.
+        results_folder (str): Folder to save the confusion matrix image.
+        session_name (str): Name of the session (used for saving the image).
     """
-    checkpoint = {
-        'model_state_dict': model.state_dict(),
-        'end_epoch': end_epoch,
-        'losses': losses,
-        'accuracies': accuracies
-    }
-    checkpoint_file = os.path.join(models_folder, f'{model_name}.pt')
-    torch.save(checkpoint, checkpoint_file)
-    print(f'Checkpoint saved: {checkpoint_file}\n')
+    # Convert numerical labels to class names using the labels map
+    true_labels = [labels_map[label] for label in true_labels]
+    predicted_labels = [labels_map[label] for label in predicted_labels]
+    classes = sorted(labels_map.values())
 
+    # Generate the confusion matrix with normalization
+    cm = confusion_matrix(true_labels, predicted_labels, labels=classes, normalize='true')
+    formatted_cm = np.array([[format_value(value) for value in row] for row in cm])
 
-def load_checkpoint(model, model_name, models_folder):
-    """
-    Loads a checkpoint for a model from a previous training.
-    This will return the model with its previous state dictionary, the epoch to continue training from and the accumulated losses and accuracies from previous training.
+    # Dynamic figure size
+    num_classes = len(classes)
+    dynamic_size = max(7.5, num_classes * 0.75)
 
-    Args:
-        model (obj): Number of images used for spikes per spike unit.
-        model_name (int): Number of images used for noise.
-        models_folder (str): The folder path name of the model.
-        
-    Returns:
-        model (obj): A PyTorch model.
-        start_epoch (int): The epoch to continue training from. 
-        losses (obj): A list of losses per epoch from training.
-        accuracies (obj): A list of accuracies per epoch from training.
-    """
-    checkpoint_file = os.path.join(models_folder, f'{model_name}.pt')
-    if os.path.exists(checkpoint_file):
-        checkpoint = torch.load(checkpoint_file)
-        model.load_state_dict(checkpoint['model_state_dict'])
-        start_epoch = checkpoint['end_epoch']
-        losses = checkpoint['losses']
-        accuracies = checkpoint['accuracies']
-        print(f'Checkpoint loaded: {checkpoint_file}\nEnded training at epoch {start_epoch}\n')
-        return model, start_epoch, losses, accuracies
-    else:
-        print('No checkpoint found.\nStart training at epoch 1\n')
-        return model, 0, [], []
+    plt.figure(figsize=(dynamic_size, dynamic_size))
+    sns.heatmap(cm, annot=formatted_cm, fmt="s", cmap='Blues', xticklabels=classes, yticklabels=classes)
+    plt.title('Confusion Matrix')
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.savefig(os.path.join(results_folder, f'{session_name}_confusion_matrix.png'))
+    
+    
+def format_value(val, threshold=0.01):
+    return '0' if val < threshold else f'{val:.2f}'
