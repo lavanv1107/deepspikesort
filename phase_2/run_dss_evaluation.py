fe4663b21a5917dc3242b4e4325367e27146feac
Vatanak (Vee) Lavan <83429771+lavanv1107@users.noreply.github.com>
2025-04-18 13:37:05 -0400

    Modified DSS pipeline running
    Created separate scripts for running DSS either for general use or for evaluation

--- phase_2/run_dss_evaluation.py
+++ phase_2/run_dss_evaluation.py
@@ -26,8 +26,13 @@ def parse_args():
 
     # Retrieve parameters from command line arguments
     parser.add_argument("recording_id", type=str, help="ID of the recording")
-    parser.add_argument("seed", type=int, help="")   
-    parser.add_argument("method", type=str, help="Method to handle coincedent spikes")  
+    parser.add_argument("min_samples", type=int, help="Minimum number of samples per unit")
+    parser.add_argument("max_samples", type=determine_count, help="Maximum number of samples per unit")
+    parser.add_argument("num_units", type=determine_count, help="Number of units")
+    parser.add_argument("seed", type=int, help="Seed for random selection of units")
+    parser.add_argument("num_samples", type=determine_count, help="Number of samples per unit")
+    parser.add_argument("noise_samples", type=determine_count, help="Number of noise samples")
+    parser.add_argument("method", type=str, help="Method to handle coincedent spikes")    
     parser.add_argument("trial_name", type=str, help="Name of the training trial")
     parser.add_argument("trial_number", type=int, help="Number of the training trial")
 
@@ -38,7 +43,10 @@ def main(args):
     # Initialize the Hugging Face Accelerator
     accelerator = Accelerator()
     
+    # Load peaks data from the file
     peaks_folder = f"../data/{args.recording_id}/peaks"
+    peaks_file = os.path.join(peaks_folder, "peaks_matched.npy")
+    peaks = np.load(peaks_file)          
     
     channels_file = f"../data/{args.recording_id}/channel_locations.npy"
     channels = np.load(channels_file)      
@@ -46,10 +54,21 @@ def main(args):
     if accelerator.is_main_process:
         print("Preparing dataset...")
 
-    # Create a trace dataset 
+    # Select units based on the parameters
+    units_selected = load_dataset.select_units(peaks, num_units=args.num_units, min_samples=args.min_samples, max_samples=args.max_samples, seed=args.seed)
+    
+    # Create an unsupervised trace dataset from selected units
     peaks_dataset = load_dataset.TraceDataset(
-        peaks_folder, seed=args.seed, channels=channels, method=args.method
+        peaks_folder, 'eval',
+        units_selected, num_samples=args.num_samples, noise_samples=args.noise_samples,
+        channels=channels, method=args.method
     )
+    
+    num_units = args.num_units
+    
+    if args.noise_samples != 0:
+        num_units = args.num_units + 1
+        units_selected = np.append(units_selected, -1)
     
     batch_size = 32
     
@@ -57,7 +76,6 @@ def main(args):
         print("Building pipeline...")
     
     # Define the CNN model
-    num_units = 100
     cnn = model.DeepSpikeSort(num_units)  
     
     # Specify loss function and optimizer
@@ -78,10 +96,24 @@ def main(args):
     
     trial_info_file = os.path.join(trial_folder, f"{trial_id}_info.txt")
     with open(trial_info_file, "w") as file:
-        file.write("Random seed: {0}\n"
-                   "Method used: {1}\n" 
-                   .format(args.seed,
-                           args.method))
+        file.write("Minimum samples per unit: {0}\n"
+                   "Maximum samples per unit: {1}\n"
+                   "Number of units: {2}\n"
+                   "Random seed: {3}\n"
+                   "Number of samples to use per unit: {4}\n"
+                   "Number of noise samples to add: {5}\n"
+                   "Method used: {6}\n" 
+                   "Selected units: \n{7}"
+                   .format(args.min_samples, 
+                           args.max_samples, 
+                           num_units, 
+                           args.seed,
+                           args.num_samples,
+                           args.noise_samples,
+                           args.method,
+                           units_selected))
+        
+    np.save(os.path.join(trial_folder, f"{trial_id}_units_selected.npy"), units_selected)
     
     # Run DeepSpikeSort
     dss = DeepSpikeSortPipeline(
