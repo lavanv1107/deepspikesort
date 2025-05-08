8422cb4c0ccbae34af75d91d239adb940a39f750
Vatanak (Vee) Lavan <83429771+lavanv1107@users.noreply.github.com>
2025-05-08 15:33:02 -0400

    Update run_dss_evaluation.py

--- phase_2/run_dss_evaluation.py
+++ phase_2/run_dss_evaluation.py
@@ -126,7 +126,7 @@ def main(args):
     if accelerator.is_main_process:
         print("Running DeepSpikeSort...")
         
-    num_epochs = 5
+    num_epochs = 100
     dss.run_deepspikesort(num_epochs)
                         
     # Free up memory 
